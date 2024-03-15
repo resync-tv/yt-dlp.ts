d09f355606f23260e0aa42a470d852a59f81c23a
vaaski <admin@vaa.ski>
2024-03-15 14:15:46 +0100

    add streamFromInfo

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import type internal from "node:stream"
 import type { YtDLP } from "../types/yt-dlp"
 export type * from "../types/yt-dlp"
 
@@ -8,4 +9,23 @@ export const getInfo = async (url: string, extraFlags: string[] = []) => {
   const data: YtDLP = JSON.parse(process.stdout)
 
   return data
+}
+
+export type GuaranteedStdio = {
+  stdin: internal.Writable
+  stdout: internal.Readable
+  stderr: internal.Readable
+}
+export const streamFromInfo = (info: YtDLP, extraFlags: string[] = []) => {
+  const infoString = JSON.stringify(info)
+  const process = execa("yt-dlp", [...extraFlags, "-o", "-", "--load-info-json", "-"])
+
+  if (!process.stdin) throw new Error("No yt-dlp stdin")
+  if (!process.stdout) throw new Error("No yt-dlp stdout")
+  if (!process.stderr) throw new Error("No yt-dlp stderr")
+
+  process.stdin.write(infoString)
+  process.stdin.end()
+
+  return process as typeof process & GuaranteedStdio
 }
